fec5c3e5a8382899356ee87f88372a6e05797b4f
fperez15 <francis.perez1590@gmail.com>
2025-02-24 14:44:58 -0500

    Navbar and header

---
+++ app/components/Header.jsx
@@ -0,0 +1,34 @@
+import { assets } from '@/assets/assets'
+import React from 'react'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Image src={assets.profile_img} alt="profile" className="rounded-full w-32"/>
+        </div>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'
+            >Hola, Soy Francis Pérez <Image src={assets.hand_icon} alt="" 
+            className="w-6"/></h3>
+
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+           frontend developer
+        </h1>
+
+        <p className='max-w-2xl mx-auto font-Ovo'>
+            Soy una forntend developer con aproximadamente 3 años de experiencia en las tecnologías principalmente de Angular y Reactjs
+           </p>
+
+           <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+             <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+                contactame <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+
+             <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+             Mi curriculum<Image src={assets.download_icon} alt='' className='w-4'/></a>
+           </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,116 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useRef } from "react";
+
+const Navbar = () => {
+
+    const sideMenuRef = useRef();
+    
+    const openMenu = () => {
+      sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+      sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
+  return (
+    <>
+    <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+      <Image src={assets.header_bg_color} alt="bg-header" className="w-full"/>
+    </div>
+
+     <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <a href="#top">
+        <Image src={assets.logo} alt="" className="w-28 cursor-pointer mr-14" />
+      </a>
+
+      <ul className="hidden md:flex items-centergap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <li>
+          <a className="font-Ovo" href="#top">
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a className="font-Ovo" href="#about">
+            Sobre mí
+          </a>
+        </li>
+        <li>
+          <a className="font-Ovo" href="#services">
+            Servicios
+          </a>
+        </li>
+        <li>
+          <a className="font-Ovo" href="#work">
+            Mí Trabajo
+          </a>
+        </li>
+        <li>
+          <a className="font-Ovo" href="#contact">
+            Contactame
+          </a>
+        </li>
+      </ul>
+
+      <div className="flex items-center">
+        <button>
+          <Image src={assets.moon_icon} alt="" className="w-6"/>
+        </button>
+
+        <a
+          href="#contacto"
+          className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
+        >
+          Contacto
+          <Image src={assets.arrow_icon} alt="contacto" className="w-3" />
+        </a>
+
+        <button className="block md:hidden ml-3" onClick={openMenu}>
+          <Image src={assets.menu_black} alt="menu" className="w-6"/>
+        </button>
+      </div>
+
+
+      {/* movile menu */}
+
+      <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+        
+        <div className="absolute right-6 top-6" onClick={closeMenu}>
+          <Image src={assets.close_black} alt="close-menu" className="w-5 cursor-pointer"/>
+        </div>
+         
+        <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#top">
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#about">
+              Sobre mí
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#services">
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#work">
+              Mí Trabajo
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" onClick={closeMenu} href="#contact">
+              Contactame
+            </a>
+          </li>
+      </ul>
+
+    </nav>
+    </>
+   
+  );
+};
+
+export default Navbar;
