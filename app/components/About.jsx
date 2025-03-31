7f52f7a15cd2e6a4dd5c525ea9b37ec803e31e2c
fperez15 <francis.perez1590@gmail.com>
2025-03-31 09:43:32 -0500

    fixed in about component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h2 className='text-center text-5xl font-Ovo'>Acerca de mí</h2>
 
         <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-            <div className='w-64 sm:w-80'>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
                 <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
             </div>
             <div className='flex-1'>
@@ -30,7 +30,7 @@ const About = () => {
 
                 <ul className='flex items-center gap-3 sm:gap-5 '>
                     {toolsData.map((tool, index) => (
-                        <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+                        <li className=' flex items-center justify-center w-12 sm:w-10 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
                             <Image src={tool} alt='Herramientas' className='w-5 sm:w-7'/>
                         </li>
                     ))}
